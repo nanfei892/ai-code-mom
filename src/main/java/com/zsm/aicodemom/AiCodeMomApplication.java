d39dc9a7ac8e9e18d2986b2a5274d92751ae7f4b
赵仕铭 <1981094442@qq.com>
2026-03-14 20:35:12 +0800

    初始化

---
+++ src/main/java/com/zsm/aicodemom/AiCodeMomApplication.java
@@ -0,0 +1,13 @@
+package com.zsm.aicodemom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeMomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeMomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zsm/aicodemom/AiCodeMomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsm.aicodemom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeMomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
